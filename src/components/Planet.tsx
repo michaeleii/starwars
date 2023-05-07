fb0c9d8b9eeda7f32a2cdc5163d54aff28404c7d
Michael Lei <michaelleie0ca@gmail.com>
2023-05-06 19:56:42 -0700

    Query for planets

---
+++ src/components/Planet.tsx
@@ -0,0 +1,12 @@
+import IPlanet from "../interfaces/IPlanet";
+
+function Planet({ planet }: { planet: IPlanet }) {
+	return (
+		<div className="card">
+			<h3>{planet.name}</h3>
+			<p>Population - {planet.population}</p>
+			<p>Terrain - {planet.terrain}</p>
+		</div>
+	);
+}
+export default Planet;
--- src/components/Planets.tsx
+++ src/components/Planets.tsx
@@ -1,7 +1,26 @@
+import { useQuery } from "react-query";
+import IPlanet from "../interfaces/IPlanet";
+import Planet from "./Planet";
+
+async function fetchPlanets() {
+	const res = await fetch("http://swapi.dev/api/planets/");
+	return res.json();
+}
 function Planets() {
+	const { data, status } = useQuery("planets", fetchPlanets);
+	console.log(data);
 	return (
 		<div>
 			<h2>Planets</h2>
+			{status === "loading" && <div>Loading data</div>}
+			{status === "error" && <div>Error fetching data</div>}
+			{status === "success" && (
+				<div>
+					{data.results.map((planet: IPlanet, i: number) => (
+						<Planet key={i} planet={planet} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
