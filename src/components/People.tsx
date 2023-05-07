6853cb59b6e8c43dcd234103d2d0b8fd2f9df4f8
Michael Lei <michaelleie0ca@gmail.com>
2023-05-06 21:33:49 -0700

    Add prev and next btn for people page

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -1,28 +1,55 @@
 import { useQuery } from "@tanstack/react-query";
 import IPerson from "../interfaces/IPerson";
 import Person from "./Person";
+import { useState } from "react";
 
-async function fetchPeople() {
-	const res = await fetch("http://swapi.dev/api/people/");
+async function fetchPeople(page: number) {
+	const res = await fetch(`http://swapi.dev/api/people/?page=${page}`);
 	return res.json();
 }
 
 function People() {
-	const { data, status } = useQuery({
-		queryKey: ["people"],
-		queryFn: fetchPeople,
+	const [page, setPage] = useState(1);
+	const { data, isLoading, isError, isPreviousData } = useQuery({
+		queryKey: ["people", page],
+		queryFn: () => fetchPeople(page),
+		keepPreviousData: true,
 	});
 	return (
 		<div>
 			<h2>People</h2>
-			{status === "loading" && <div>Loading data...</div>}
-			{status === "error" && <div>Error fetching data</div>}
-			{status === "success" && (
-				<div>
-					{data.results.map((person: IPerson, i: number) => (
-						<Person key={i} person={person} />
-					))}
-				</div>
+			{isLoading ? (
+				<div>Loading data...</div>
+			) : isError ? (
+				<div>Error fetching data</div>
+			) : (
+				<>
+					<div>
+						{data.results.map((person: IPerson, i: number) => (
+							<Person key={i} person={person} />
+						))}
+					</div>
+					<div className="btn-container">
+						<button
+							onClick={() => setPage((old) => Math.max(old - 1, 0))}
+							disabled={page === 1}
+						>
+							Previous Page
+						</button>{" "}
+						<span>{page}</span>
+						<button
+							onClick={() => {
+								if (!isPreviousData && data.next) {
+									setPage((old) => old + 1);
+								}
+							}}
+							// Disable the Next Page button until we know a next page is available
+							disabled={isPreviousData || !data.next}
+						>
+							Next Page
+						</button>
+					</div>
+				</>
 			)}
 		</div>
 	);
